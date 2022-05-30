a591406a56f9f61e1ba44b9f77b6fb1f796457e4
Richard Piazza <richard@richardpiazza.com>
2022-05-30 15:52:23 -0500

    Fixes for tvOS limited Directory Access

--- Sources/Occurrence/Extensions/FileManager+Occurrence.swift
+++ Sources/Occurrence/Extensions/FileManager+Occurrence.swift
@@ -3,7 +3,11 @@ import Foundation
 extension FileManager {
     /// Locates (and creates) the **Occurrence** package directory.
     func occurrenceDirectory() throws -> URL {
+        #if os(tvOS)
+        let applicationSupport = try url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        #else
         let applicationSupport = try url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        #endif
         let directory = applicationSupport.appendingPathComponent("Occurrence", isDirectory: true)
         if !fileExists(atPath: directory.path) {
             try createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
