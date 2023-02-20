23046fadc8106bad312692e9c9b8d1f506d079dc
Richard Piazza <richard@richardpiazza.com>
2023-02-20 11:20:16 -0600

    Internal SQLite References (#14)
    Updates references to SQLite packages

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.5.2")),
-        .package(name: "PerfectSQLite", url: "https://github.com/richardpiazza/Perfect-SQLite", .upToNextMajor(from: "5.1.1")),
-        .package(url: "https://github.com/richardpiazza/Statement.git", .upToNextMajor(from: "0.7.1"))
+        .package(url: "https://github.com/richardpiazza/Statement.git", .upToNextMajor(from: "0.7.1")),
+        .package(url: "https://github.com/richardpiazza/Perfect-SQLite", .upToNextMajor(from: "5.1.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -34,7 +34,7 @@ let package = Package(
                 .product(name: "Logging", package: "swift-log"),
                 "Statement",
                 .product(name: "StatementSQLite", package: "Statement"),
-                "PerfectSQLite"
+                .product(name: "PerfectSQLite", package: "Perfect-SQLite"),
             ]
         ),
         .testTarget(
